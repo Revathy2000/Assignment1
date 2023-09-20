54788015fea9ad48bb53f53311f19b8103afda36
Revathy2000 <revathyr1914@gmail.com>
2023-09-20 21:41:26 +0100

    first commit

---
+++ oddeven.js
@@ -0,0 +1,11 @@
+// Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, 
+// check if the current number is odd or even, and display the message to the screen as odd or even.
+
+for(var x = 0; x <= 15; x++){
+    if(x % 2 == 0){
+        console.log(x+ "is even")
+    }
+    else{
+        console.log(x+ "is odd")
+    }
+  }
